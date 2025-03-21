573805866f643e26ca5ae5811334d69ccee0f37f
m01310g <m01310g@gmail.com>
2025-03-21 16:47:43 +0900

    [rename] 03/21 프로젝트 섹션 폴더 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import AboutSection from "./components/AboutSection";
 import Header from "./components/header/Header";
 import IntroduceSection from "./components/introduce/IntroduceSection";
-import ProjectsSection from "./components/ProjectsSection";
+import ProjectsSection from "./components/projects/ProjectsSection";
 import SkillsSection from "./components/skills/SkillsSection";
 import { Main } from "./page.styled";
 
