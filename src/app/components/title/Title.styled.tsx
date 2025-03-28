f39a7a5e3f8bc51b0c31b21a6e58af838cf9b2d2
m01310g <m01310g@gmail.com>
2025-03-28 22:12:15 +0900

    [design] 03/28 항목별 제목 글자 크기 변경

--- src/app/components/title/Title.styled.tsx
+++ src/app/components/title/Title.styled.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const StyledH1 = styled.h1`
   color: var(--secondary-color);
   font-family: "DungGeunMo";
+  font-weight: 500;
+  font-size: var(--font-size-xxl);
 
   span {
     color: var(--skyblue-color);
