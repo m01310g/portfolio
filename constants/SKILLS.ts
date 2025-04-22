5d48b56bb7e9268c0b623e21e71f95ad318db953
m01310g <m01310g@gmail.com>
2025-04-22 12:01:40 +0900

    [chore] 04/22 스킬 내용 수정

--- constants/SKILLS.ts
+++ constants/SKILLS.ts
@@ -1,21 +1,23 @@
 export const SKILLS = {
   familiar: [
-    { name: "React.js", icon: "devicon-react-original colored" },
-    { name: "JavaScript(ES6+)", icon: "devicon-javascript-plain colored" },
-    { name: "HTML5", icon: "devicon-html5-plain colored" },
-    { name: "CSS3", icon: "devicon-css3-plain colored" },
+    { name: "React", icon: "devicon-react-original colored" },
+    { name: "TypeScript", icon: "devicon-typescript-plain colored" },
+    { name: "Next.js", icon: "" },
+
     {
       name: "Styled-Components",
       icon: "",
     },
+    { name: "Git", icon: "" },
     { name: "GitHub", icon: "devicon-github-original colored" },
   ],
   experienced: [
-    { name: "TypeScript", icon: "devicon-typescript-plain colored" },
-    { name: "Next.js", icon: "" },
-    { name: "jQuery", icon: "devicon-jquery-plain colored" },
+    { name: "JavaScript(ES6+)", icon: "devicon-javascript-plain colored" },
+    { name: "HTML5", icon: "devicon-html5-plain colored" },
+    { name: "CSS3", icon: "devicon-css3-plain colored" },
     { name: "Zustand", icon: "" },
     { name: "MySQL", icon: "devicon-mysql-plain colored" },
-    { name: "PostgreSQL", icon: "devicon-postgresql-plain colored" },
+    { name: "Prisma", icon: "devicon-prisma-plain colored" },
+    { name: "Supabase", icon: "devicon-supabase-plain colored" },
   ],
 };
