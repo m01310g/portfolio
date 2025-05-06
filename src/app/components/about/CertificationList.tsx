cf04c1bd094453cb79f037e1434802bcbd673a7a
m01310g <m01310g@gmail.com>
2025-05-06 13:16:43 +0900

    [chore] 05/06 어학 성적 입력

--- src/app/components/about/CertificationList.tsx
+++ src/app/components/about/CertificationList.tsx
@@ -8,6 +8,10 @@ const CertificationList = () => {
         <h3>정보처리기사</h3>
         <p>한국산업인력공단, 2024년 9월 취득</p>
       </CertificationItem>
+      <CertificationItem>
+        <h3>TOEIC</h3>
+        <p>ETS, 2025년 5월</p>
+      </CertificationItem>
     </AboutContent>
   );
 };
